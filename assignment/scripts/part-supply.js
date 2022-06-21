6d0d680ed6f828ffeabeef7b126d8a3abd91179b
Linssi Colin Ruiz <linssi101@yahoo.com>
2022-06-20 23:27:29 -0500

    updated assignment

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -3,25 +3,30 @@ console.log('****** Part Supply *******');
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
-console.log('1. Number of partsNeeded:');
+console.log('1. Number of partsNeeded:', '40' );
 
 var partsNeeded = '40';
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+console.log('2. Array of supplyChanges:' );
 
-let supplyChanges = [ '3', '5', '-6', '0', '7', '11' ];
+let supplyChanges = [ '3' , '5', '-6', '0', '7', '11' ];
+console.log (supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
+console.log('3. Second supplyChange is:', supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+console.log('4. Removed item:', '11' );
+
+let removedItem = supplyChanges.pop();
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+
+let addItem = supplyChanges.push('25');
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -30,7 +35,16 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
 
-
+for (let i = 0; i < supplyChanges.length; i++) {
+    const change = supplyChanges[i]; 
+    if (change === 0) {
+        console.log('Added x parts');
+    } else if (change < 0) {
+        console.log('No change')
+    } else if (change > 0) {
+        console.log('Removed x parts')
+    }
+}
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
